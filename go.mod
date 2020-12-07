5e5f70ed054c1067a9683d01ee0a1ce13938ba14
Konstantin Morozov <morozov.cookie@gmail.com>
2020-12-07 12:57:04 +0300

    feat(parser): implement xpath parser

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/antchfx/htmlquery v1.2.3
+	github.com/stretchr/testify v1.6.1
 	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd
 )
---
+++ xpath/parser_test.go
@@ -0,0 +1,67 @@
+package xpath
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParser_Parse(t *testing.T) {
+	tt := []struct {
+		name    string
+		enabled bool
+
+		r          io.Reader
+		expression string
+
+		wantErr bool
+
+		expectedNodes []string
+	}{
+		{
+			name: "pass",
+			enabled: true,
+
+			r: bytes.NewBufferString(`<ul>
+				<li>Tag attributes</li>
+				<li>Make plain text</li>
+			</ul>`),
+			expression: `//ul/li`,
+
+			expectedNodes: []string{
+				`<li>Tag attributes</li>`,
+				`<li>Make plain text</li>`,
+			},
+		},
+		{
+			name: "query error",
+			enabled: true,
+
+			r: bytes.NewBufferString(`<ul>
+				<li>Tag attributes</li>
+				<li>Make plain text</li>
+			</ul>`),
+			expression: string([]byte(nil)),
+
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tt {
+		t.Run(test.name, func(t *testing.T) {
+			if !test.enabled {
+				t.SkipNow()
+			}
+
+			actualNodes, err := NewParser(test.expression).Parse(test.r)
+			if (err != nil) != test.wantErr {
+				t.Error(err)
+				t.FailNow()
+			}
+
+			assert.Equal(t, test.expectedNodes, actualNodes)
+		})
+	}
+}
